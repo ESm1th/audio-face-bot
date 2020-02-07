8f61463fabb2b8e622240882d62d96bcf14fc29f
Evgeny <ku7nec0v@gmail.com>
2020-02-08 00:45:46 +0300

    delete: django project was deleted

--- telegram/telegram/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('', include('bot.urls', namespace='bot')),
-    path('admin/', admin.site.urls),
-]
